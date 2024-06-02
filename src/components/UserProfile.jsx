fe2d6ecd24914638bb8841b66dc5c8006769b036
Joohwan Seo <songqkaoswe@naver.com>
2024-06-02 22:24:43 +0900

    delete/ github-login

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UserProfile = ({ user }) => {
-  const posts = ['작성한 글 1', '작성한 글 2', '작성한 글 3'];
-
-  const handleLogin = () => {
-    const clientId = 'Ov23liCvjtEAYP4bgksn';
-    const redirectUrl = 'http://localhost:5174/';
-    const githubURL = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}`;
-    window.location.href = githubURL;
-  };
+  const posts = [
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 ',
+    '작성한 글 '
+  ];
+  // const reasonToUse = [ reason, setReason] = useState('')
 
   return (
     <div style={{ display: 'flex', padding: '20px' }}>
@@ -17,9 +26,7 @@ const UserProfile = ({ user }) => {
         <h2>{user.username}</h2>
         <span>Followers: {user.follower}</span>
         <span>Following: {user.following}</span>
-        <button onClick={handleLogin}>깃헙 로그인</button>
-        <button>구글 로그인</button>
-        <button>네이버 로그인</button>
+        <input />
       </section>
       <section style={{ overflowY: 'scroll', height: '500px', width: '70%', padding: '20px' }}>
         {posts.map((post, index) => (
