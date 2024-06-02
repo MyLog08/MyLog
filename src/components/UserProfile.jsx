6bd6047fd7fcb278ec57dfe3f138489c5d1e23e4
Joohwan Seo <songqkaoswe@naver.com>
2024-06-02 21:21:56 +0900

    fix/깃허브 소셜 로그인 연결 완료

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -3,32 +3,30 @@ import React from 'react';
 const UserProfile = ({ user }) => {
   const posts = ['작성한 글 1', '작성한 글 2', '작성한 글 3'];
 
-  const GithubBotton = () => {
+  const handleLogin = () => {
     const clientId = 'Ov23liCvjtEAYP4bgksn';
     const redirectUrl = 'http://localhost:5174/';
     const githubURL = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}`;
-    const handleLogin = () => {
-      window.location.herf = githubURL;
-    };
+    window.location.href = githubURL;
   };
 
   return (
     <div style={{ display: 'flex', padding: '20px' }}>
       <section style={{ width: '30%', padding: '20px', borderRight: '1px solid #ddd' }}>
-        <img src="{profile.profilePic}" alt="Profile" style={{ width: '100%' }} />
+        <img src={user.profilePic} alt="Profile" style={{ width: '100%' }} />
         <h2>{user.username}</h2>
         <span>Followers: {user.follower}</span>
-        <span>Following:{user.following}</span>
+        <span>Following: {user.following}</span>
         <button onClick={handleLogin}>깃헙 로그인</button>
         <button>구글 로그인</button>
         <button>네이버 로그인</button>
       </section>
       <section style={{ overflowY: 'scroll', height: '500px', width: '70%', padding: '20px' }}>
-        {posts.map((post, index) => {
+        {posts.map((post, index) => (
           <div key={index} style={{ margin: '10px 0', padding: '10px', border: '1px solid #ddd' }}>
             <span>{post}</span>
-          </div>;
-        })}
+          </div>
+        ))}
       </section>
     </div>
   );
