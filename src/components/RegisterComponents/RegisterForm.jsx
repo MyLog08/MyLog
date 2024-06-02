f8561d1067820237075ea94d91584539e0874f88
muyeon <gmakin36@gmail.com>
2024-06-02 17:23:50 +0900

    feat: supabase user registration feature - supabase 사용자 등록 추가 - TODO: 사용자 validation 추가 - TODO: 회원가입시 바로 로그인 될 수 있도록 처리

--- src/components/RegisterComponents/RegisterForm.jsx
+++ src/components/RegisterComponents/RegisterForm.jsx
@@ -1,4 +1,5 @@
 import useFormInputs from '../../hooks/useInput';
+import supabase from '../../supabase/supabase';
 import Button from '../Button';
 import Input from '../Input';
 
@@ -12,14 +13,51 @@ function RegisterForm() {
     confirm: ''
   };
 
-  const { inputs, setInputs, handleOnChange, handleResetInputs } = useFormInputs(initialState);
+  const { inputs, handleOnChange, handleResetInputs } = useFormInputs(initialState);
 
   const { name, email, nickname, birth, password, confirm } = inputs;
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    // TODO 유효성 검사 변경해야함.
+    if (password !== confirm) {
+      alert('비밀번호 불일치');
+      return;
+    }
+
+    try {
+      // TODO 테스트용 date 포메팅
+      const date = new Date(Date.UTC(2020, 11, 20, 3, 23, 16, 738));
+      const { data, error } = await supabase.from('Users').insert({
+        name,
+        email,
+        nickname,
+        birth,
+        password,
+        createdAt: date,
+        updatedAt: date
+      });
+
+      console.log(data);
+
+      if (error) {
+        alert(error.message);
+        return;
+      }
+
+      alert('회원가입 완료');
+      handleResetInputs();
+    } catch (err) {
+      console.error(err);
+      alert('회원가입 중 오류 발생');
+    }
+  };
 
   return (
     <div>
       <h1>회원가입</h1>
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <Input placeholder="이름" value={name} name="name" id="name" onChange={handleOnChange} />
         <Input placeholder="이메일" value={email} name="email" id="email" onChange={handleOnChange} />
         <Input placeholder="닉네임" value={nickname} name="nickname" id="nickname" onChange={handleOnChange} />
