d821ffe9c5064c45aa6988d88e2671c5733b0a13
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-06-02 19:04:51 +0900

    feat: get and display article information

--- src/components/ArticleDisplay.jsx
+++ src/components/ArticleDisplay.jsx
@@ -1,17 +1,84 @@
+import { useState, useEffect } from 'react';
+import supabase from '../supabase/supabase';
+
 const ArticleDisplay = () => {
+  // 테스트용 게시글 아이디
+  const articleId = '6e629508-8e0e-4ea7-b4b8-d9a44f4a8f96';
+
+  const [article, setArticle] = useState(null);
+  const [userNickname, setUserNickname] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // 게시글 정보 가져오기
+      const { data: articleData, error: articleError } = await supabase
+        .from('Articles')
+        .select('*')
+        .eq('articleId', articleId)
+        .single();
+      if (articleError) {
+        console.log(articleError);
+        return;
+      }
+      const fetchedArticle = articleData;
+
+      // 작성자 닉네임 가져오기
+      const { data: userNicknameData, error: userNicknameError } = await supabase
+        .from('Users')
+        .select('nickname')
+        .eq('userId', fetchedArticle.userId)
+        .single();
+      if (userNicknameError) {
+        console.log(userNicknameError);
+        return;
+      }
+      const fetchedNickname = userNicknameData;
+
+      setArticle(fetchedArticle);
+      setUserNickname(fetchedNickname);
+    };
+
+    fetchData();
+  }, [articleId]);
+
+  if (!article || !userNickname) {
+    // 나중에 로딩 화면 스피너 넣으면 좋을 것 같습니다!
+    return <div>로딩 중...</div>;
+  }
+
   return (
     <section>
-      <h1>{/* 게시글 타이틀 */}</h1>
+      <h1>{article.title}</h1>
       <div>
         <div>
-          <span>{/* 게시글 작성자 닉네임 */}</span>
-          <span>{/* 게시글 작성 날짜 */}</span>
-          <span>{/* 게시글 좋아요 수 */}</span>
+          <span>{userNickname.nickname}</span>
+          <span>
+            {article.createdAt === article.updatedAt
+              ? new Date(article.createdAt).toLocaleDateString('ko-KR', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                })
+              : new Date(article.updatedAt).toLocaleDateString('ko-KR', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                })}
+          </span>
+          <span>♥ {article.like}</span>
         </div>
         <div>
           <button>팔로우</button>
         </div>
-        <div>{/* 게시글 내용 */}</div>
+        <div>
+          <div>
+            {JSON.parse(article.imageUrl).map((url, index) => (
+              <img key={index} src={url} alt="이미지" />
+            ))}
+          </div>
+          {/* 스타일 작업 시 아래 스타일 적용 부탁드립니다! */}
+          <div style={{ whiteSpace: 'pre-wrap' }}>{article.content}</div>
+        </div>
       </div>
     </section>
   );
