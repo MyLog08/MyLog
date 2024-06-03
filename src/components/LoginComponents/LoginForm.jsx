ee7f0b0d1cc240060b96b0fc2d83bc43d2634464
muyeon <gmakin36@gmail.com>
2024-06-03 11:59:59 +0900

    feat: configuring login pages and components - 로그인 페이지 뼈대 작성 - TODO: 유저 로그인 인증 및 인가 - TODO: 로그인 유효성 검사 - TODO: 비밀번호 찾기 - TODO: 소셜 로그인

--- src/components/LoginComponents/LoginForm.jsx
+++ src/components/LoginComponents/LoginForm.jsx
@@ -1,5 +1,41 @@
+import useFormInputs from '../../hooks/useInput';
+import Button from '../Button';
+import Input from '../Input';
+
 function LoginForm() {
-  return <div>LoginForm</div>;
+  const initialState = {
+    email: '',
+    password: ''
+  };
+
+  const { inputs, handleOnChange, handleResetInputs } = useFormInputs(initialState);
+
+  const { email, password } = inputs;
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    alert('로그인 완료');
+    handleResetInputs();
+  };
+
+  return (
+    <div>
+      <h1>로그인</h1>
+      <form onSubmit={handleOnSubmit}>
+        <Input placeholder="이메일" value={email} name="email" id="email" onChange={handleOnChange} />
+        <Input
+          placeholder="비밀번호"
+          type="password"
+          value={password}
+          name="password"
+          id="password"
+          onChange={handleOnChange}
+        />
+        <Button value="로그인하기" />
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
