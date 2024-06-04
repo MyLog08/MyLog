0a5b1dac422732d6ea75165ae2b6ec421dd61dfe
Joohwan Seo <songqkaoswe@naver.com>
2024-06-04 14:55:24 +0900

    fix/ supabase 데이터 호출 코드 수정

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -2,25 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { updateUserProfile } from '../redux/slices/userProfileSlice';
-import supabase  from '../supabase/supabase'
+import supabase from '../supabase/supabase';
+import { v4 as uuidv4 } from 'uuid';
+
+const getInitialInputs = (user) => ({
+  name: user.userName || '',
+  nickname: '',
+  profilePicture: user.profilePicture || '',
+  currentPassword: '',
+  newPassword: '',
+  confirmPassword: '',
+  mylogReason: ''
+})
 
 function EditProfile() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { user, status, error } = useSelector((state) => state.userProfile);
-  const [inputs, setInputs] = useState({
-    name: user.userName || '',
-    nickname: '',
-    profilePicture: user.profilePicture || '',
-    currentPssword: '',
-    newPassword: '',
-    confirmPassword: '',
-    mylogReason:'',
-  });
-// 프로필 페이지 이동
+  const [inputs, setInputs] = useState(() => getInitialInputs(user));
+  // 프로필 페이지 이동
   useEffect(() => {
     if (status === 'succeeded') {
-      navigate('/profile'); 
+      navigate('/profile');
     }
   }, [status, navigate]);
 
@@ -32,34 +35,68 @@ function EditProfile() {
     });
   };
 
-  const handleSubmit = (e) => {
+  // const profilePicture = e.target.files[0];
+
+  const handleChangeProfile = async (file) => {
+    if (!file) return
+
+    const { data: uploadData, error: uploadError } = await supabase
+    .storage
+    .from('Users')
+    .upload(`public/${uuidv4()}`, file)
+
+    if (uploadError) {
+      console.log(uploadError)
+      return
+    }
+
+    const {publicURL, error: urlError} = supabase.storage
+    .from('Users')
+    .getPublicUrl(uploadData.Key)
+
+    if (urlError) {
+      console.log(urlError)
+      return
+    }
+    return publicURL
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (inputs.newPassword !== inputs.confirmPassword) {
       alert('새 비밀번호가 일치하지 않습니다!');
       return;
     }
 
+    const fileElement = document.getElementById('profilePicture')
+    const file = fileElement.files[0]
+    const profilePictureUrl = await handleChangeProfile(file)
+
+    if (profilePictureUrl) {
+      inputs.profilePicture = profilePictureUrl;
+    }
 
     dispatch(updateUserProfile({ userId: user.id, updates: inputs }));
   };
 
   // 뒤로가기
   const handleBack = () => {
-    navigate(-1); // 이전 페이지로 이동
+    navigate(-1); 
   };
 
   // 탈퇴하기
   const handleDeleteAccount = async () => {
     const { error } = await supabase
-      .from('Users') 
-      .delete()
-      .eq('id', user.id); 
+    .from('Users')
+    .delete()
+    .eq('id', user.id);
 
     if (error) {
       alert('계정 삭제 중 오류가 발생했습니다: ' + error.message);
     } else {
       alert('계정이 성공적으로 삭제되었습니다.');
-      navigate('/'); // 홈 페이지로 이동
+      navigate('/'); // 탈퇴하기 성공 시, 홈으로 이동
     }
   };
 
@@ -68,41 +105,29 @@ function EditProfile() {
       <button onClick={handleBack}>뒤로가기</button>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">이름:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={inputs.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="nickname">닉네임:</label>
-          <input
-            type="text"
-            id="nickname"
-            name="nickname"
-            value={inputs.nickname}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
           <label htmlFor="profilePicture">프로필 사진:</label>
           <input
             type="file"
             id="profilePicture"
             name="profilePicture"
-            value={inputs.profilePicture}
-            onChange={handleChange}
+            // value={inputs.profilePicture}
+            onChange={(e) => handleChangeProfile(e.target.files[0])}
           />
+        </div>
+        <div>
+          <label htmlFor="name">이름:</label>
+          <input type="text" id="name" name="name" value={inputs.name} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="nickname">닉네임:</label>
+          <input type="text" id="nickname" name="nickname" value={inputs.nickname} onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="currentPassword">현재 비밀번호:</label>
           <input
             type="password"
             id="currentPassword"
-            name="profilePicture"
+            name="currentPassword"
             value={inputs.currentPssword}
             onChange={handleChange}
           />
@@ -128,19 +153,26 @@ function EditProfile() {
           />
         </div>
         <div>
-        <label htmlFor="reasonToUseBlog">Mylog 운영 이유:</label>
+          <label htmlFor="reasonToUseBlog">Mylog 운영 이유:</label>
           <input
             type="text"
-            id='reasonToBlog'
-            name='mylogReason'
+            id="reasonToBlog"
+            name="mylogReason"
             value={inputs.mylogReason}
-            onChange={handleChange}/>
-            
+            onChange={handleChange} />
         </div>
         {status === 'failed' && <p>{error}</p>}
-        <button type="submit" style={{color:'blue'}}>변경 완료</button>
+        <button
+          type="submit"
+          style={{ color: 'blue' }}
+          onClick={handleChangeProfile}
+          >          
+          변경 완료
+        </button>
       </form>
-      <button onClick={handleDeleteAccount} style={{color:'red'}}>탈퇴하기</button>
+      <button onClick={handleDeleteAccount} style={{ color: 'red' }}>
+        탈퇴하기
+      </button>
     </>
   );
 }
