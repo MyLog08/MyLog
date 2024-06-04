35af0ac61c62eb7f25a3c8cc4006aa917258a7ca
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-06-04 21:46:23 +0900

    add: set up article submit page with component

---
+++ src/components/SubmitPage/ArticleForm.jsx
@@ -0,0 +1,5 @@
+const ArticleForm = () => {
+  return <></>;
+};
+
+export default ArticleForm;
---
+++ src/pages/SubmitPage/ArticleSubmitPage.jsx
@@ -0,0 +1,11 @@
+import ArticleForm from '../../components/SubmitPage/ArticleForm';
+
+const ArticleSubmitPage = () => {
+  return (
+    <>
+      <ArticleForm />
+    </>
+  );
+};
+
+export default ArticleSubmitPage;
