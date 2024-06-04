46780c74056dfe1ed7d1ffa45f4b1b744df6bd5c
Joohwan Seo <songqkaoswe@naver.com>
2024-06-04 20:02:01 +0900

    fix/ call data fixing

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -9,14 +9,17 @@ const UserProfile = ({ user }) => {
   //ID 를 이용해 Article 테이블에 있는 userId 와 일치하는 데이터들을 찾아서 담는다.
   //이를 보여준다.
   useEffect(() => {
-    fetchPosts(user.id)
+    fetchPosts('01b7a810-c59c-4b70-b210-75f7d74c6deb')
   }, [user.id])
 
   const fetchPosts = async (userId) => {
-   const {data, error} = await supabase
+   console.log(userId)
+    const {data, error} = await supabase
    .from('Articles')
-   .select(`articleId, title,content,imageUrl,userId,Users(userId)`)
+  //  .select(`articleId, title,content,imageUrl,userId,Users(userId)`)
+  .select('*')
    .eq('userId', userId)
+   console.log(data)
 
    if (error) {
     console.log('연결할 수 없습니다', error)
