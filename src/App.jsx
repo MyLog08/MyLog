55325657a605a3482ed79d20d33c7968276c1a40
Joohwan Seo <songqkaoswe@naver.com>
2024-06-04 20:18:41 +0900

    delete/ 주석코드삭제,fix/supabase 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,7 @@ import supabase from './supabase/supabase';
 const App = () => {
   const [user, setUser] = useState([]);
   const [page, setPage] = useState('mypage'); // 페이지 상태 관리 추가
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const { data, error } = await supabase
-  //     .from('Users').select('*');
-  //     if (error) {
-  //       console.log(error);
-  //     } else {
-  //       console.log(data);
-  //       setUser(data);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
+  
   const handleUpdateProfile = (updateUser) => {
     setUser(updateUser);
     setPage('mypage');
