4f211bd77f734ac5a90bae7b5c5bdf75b98e2a11
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-06-04 11:16:36 +0900

    feat: add login check

--- src/components/DetailPage/CommentInput.jsx
+++ src/components/DetailPage/CommentInput.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkSignIn } from '../../redux/slices/authSlice';
 import supabase from '../../supabase/supabase';
 import dayjs from 'dayjs';
 
@@ -9,7 +11,13 @@ const CommentInput = () => {
   const [commentsCount, setCommentsCount] = useState(0);
   const [content, setContent] = useState('');
 
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
   useEffect(() => {
+    dispatch(checkSignIn());
+
     const fetchData = async () => {
       try {
         // 댓글 수 가져오기
@@ -29,28 +37,29 @@ const CommentInput = () => {
     };
 
     fetchData();
-  }, [articleId]);
+  }, [dispatch, articleId]);
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      // 로그인/비로그인 체크
-
       // 비로그인 시 로그인 페이지로 리다이렉션
+      if (!isLoggedIn) {
+        alert('로그인 후 댓글을 작성할 수 있습니다.');
+        window.location.href = '/login';
+        return;
+      }
 
       // 유효성 검사 : 댓글 5자 이상
-      if (content.trim() === '' || content.length < 5) {
+      if (content.trim().length < 5) {
         alert('댓글을 5자 이상 입력해 주세요.');
         return;
       }
 
       // 로그인 시 댓글 등록
-      // commentId, userId, articleId, content, createdAt, updatedAt
-      const { commentData, commentError } = await supabase.from('Comments').insert({
+      const { commentError } = await supabase.from('Comments').insert({
         commentId: crypto.randomUUID(),
-        // 테스트용
-        userId: '00113606-fac2-45ca-b8b6-ab2ae57c263d',
+        userId: user.id,
         articleId,
         content,
         createdAt: dayjs().format('YYYY-MM-DD hh:mm:ss'),
