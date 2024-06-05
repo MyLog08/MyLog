7f2309b03cc85ef4aaf5cba746b27129d447f161
Joohwan Seo <songqkaoswe@naver.com>
2024-06-05 22:31:04 +0900

    problem/ 데이터 호출 문제

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,29 +4,29 @@ import { useNavigate } from 'react-router-dom';
 import supabase from '../supabase/supabase';
 import { checkSignIn } from '../redux/slices/authSlice';
 
-const UserProfile = () => {
-  /**
-   * 1. 리덕스의 useDispatch 를 사용해서 유저에 대한 정보를 로드한다. 이때 useEffect 와 함께 사용하고
-   * 2. checkSign() 요거 쓰면 댐
-   * 3. 리덕스의 useSeletor 를 사용해서 유저의 정보를 가져온다.
-   * 4. 가져온 유저의 정보를 변수 user 에 저장한다.
-   * 5. user 변수를 통해 sup
-   * 
-   * 1. userId 를 통해 데이터베이스에 접근(데이터베이스에 접근하는 함수)해서 article 들을 가져온다.abase 에 접근해 해당 유저의 모든 정보를 가져온다.
-   * 2. 이 정보를 화면에 그려준다.
-  * 
+/**
+ * 1. 리덕스의 useDispatch 를 사용해서 유저에 대한 정보를 로드한다. 이때 useEffect 와 함께 사용하고
+ * 2. checkSign() 요거 쓰면 댐
+ * 3. 리덕스의 useSeletor 를 사용해서 유저의 정보를 가져온다.
+ * 4. 가져온 유저의 정보를 변수 user 에 저장한다.
+ * 5. user 변수를 통해 sup
+ * 
+ * 1. userId 를 통해 데이터베이스에 접근(데이터베이스에 접근하는 함수)해서 article 들을 가져온다.abase 에 접근해 해당 유저의 모든 정보를 가져온다.
+ * 2. 이 정보를 화면에 그려준다.
+* 
 6. 이 정보를 화면에 그려준다.
 
-  
-
   // auth/login
-   */
+// 로그인 정보를 받아와야 된다.
+ */
+const UserProfile = () => {
 
   const user = useSelector((state) => state.auth.user);
   const [articles, setArticles] = useState([]);
-
-  console.log(user);
   const dispatch = useDispatch(); // 우리는 쓰기/삭제/업데이트 를 할때 사용해요. 저희는 함수동작을 위해 사용하고있어요.
+  const navigate = useNavigate();
+  const [dataUser, setDataUser] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     dispatch(checkSignIn());
@@ -34,13 +34,17 @@ const UserProfile = () => {
 
   useEffect(() => {
     const fetchArticles = async () => {
+      if (!user?.id) return;
+
       try {
-        const { data, error } = await supabase.from('Articles').select('*').eq('userId', user.id);
+        const { data, error } = await supabase
+        .from('Articles')
+        .select('*')
+        .eq('userId', user.id);
 
         if (error) {
           console.log(error);
         } else {
-          console.log(data);
           setArticles(() => data);
         }
       } catch (err) {
@@ -52,28 +56,48 @@ const UserProfile = () => {
   }, [user]);
 
   useEffect(() => {
-    if (user) {
-      console.log(`아티클즈임 : ${JSON.stringify(articles)}`);
-      console.log(`아티클즈임: ${articles[0].articleId}`);
-    }
-  }, [articles]);
-  //
+    const textId = async () => {
+      if (!user?.id) return;
+      try {
+        const { data, error } = await supabase.from('Users').select('*').eq('userId', user.id);
+
+        if (error) {
+          console.log(error);
+        } else {
+          setDataUser(() => data);
+        }
+      } catch (err) {
+        console.error('Error:', err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    textId();
+  }, [user]);
+
+  const moveToEdit = () => {
+    navigate('/editprofile');
+  };
+  console.log('data =', dataUser);
+  console.log('user =', user);
+
+  if (loading) {
+    return <p>로딩중</p>;
+  }
 
   return (
     <div style={{ display: 'flex', padding: '20px' }}>
       {user ? (
         <>
           <section style={{ width: '30%', padding: '20px', borderRight: '1px solid #ddd' }}>
-            <img src={`${user.user_metadata.avatar_url}`} alt="Profile" style={{ width: '100%' }} />
-            <h2>{user.user_metadata.name}</h2>
-            <span style={{ display: 'flex' }}>Followers : {'1'}</span>
-            <span>Following : {'1'}</span>
+            <img src={''} alt="Profile" style={{ width: '100%' }} />
+            <h2>{dataUser.name}</h2>
             <h5>`'Mylog 운영 이유: '아직 계획이 없습니다' </h5>
-            <button>프로필 편집</button>
+            <button onClick={moveToEdit}>프로필 편집</button>
           </section>
           <section style={{ overflowY: 'scroll', height: '500px', width: '70%', padding: '20px' }}>
             {articles.map((article) => (
-              <div key={article.articleId}  style={{ margin: '10px 0', padding: '10px', border: '1px solid #ddd' }}>
+              <div key={article.articleId} style={{ margin: '10px 0', padding: '10px', border: '1px solid #ddd' }}>
                 <h3>{article.title}</h3>
                 <p>{article.content}</p>
               </div>
--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -2,107 +2,100 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import supabase from '../supabase/supabase';
-
 import { v4 as uuidv4 } from 'uuid';
-import { name } from 'tar/types';
+import useFormInputs from '../hooks/useInput';
+import { checkSignIn } from '../redux/slices/authSlice';
 
-const getInitialInputs = (user) => ({
-  name: user.userName || '',
+const initialState = {
+  name: '',
   nickname: '',
-  profilePicture: user.profilePicture || '',
   currentPassword: '',
-  newPassword: '',
-  confirmPassword: '',
-  mylogReason: ''
-})
+  mylogReason: '',
+  profilePicture: ''
+};
 
 function EditProfile() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { user, status, error } = useSelector((state) => state.userProfile);
-  const [inputs, setInputs] = useState(() => getInitialInputs(user));
+  // const { user, status, error } = useSelector((state) => state.userProfile);
+  const user = useSelector((state) => state.auth.user);
+  const { inputs, setInputs, handleOnChange, handleResetInputs } = useFormInputs(initialState);
+  const { name, nickname, currentPassword, mylogReason, profilePicture } = inputs;
+  const [picture, setPicture] = useState('');
 
   // 프로필 페이지 이동
-  useEffect(() => {
-    if (status === 'succeeded') {
-      navigate('/profile');
-    }
-  }, [status, navigate]);
+  // useEffect(() => {
+  //   if (status === 'succeeded') {
+  //     navigate('/profile');
+  //   }
+  // }, [status, navigate]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
+  useEffect(() => {
+    dispatch(checkSignIn());
+  }, [dispatch]);
 
   const handleUploadProfile = async (file) => {
-    if (!file) return
+    if (!file) return;
 
-    const { data: uploadData, error: uploadError } = await supabase
-    .storage
-    .from('Users')
-    .upload(`public/${uuidv4()}`, file)
+    const { data, error } = await supabase.storage.from('images').upload(`/${uuidv4()}`, file);
 
-    if (uploadError) {
-      console.log(uploadError)
-      return
+    if (error) {
+      console.log(error);
+      return;
     }
-
-
-    const {publicURL, error: urlError} = supabase.storage
-    .from('Users')
-    .getPublicUrl(uploadData.Key)
-
+    console.log(data);
+    const { data: publicURL, error: urlError } = await supabase.storage.from('images').getPublicUrl(data.path);
     if (urlError) {
-      console.log(urlError)
-      return
+      console.log(urlError);
+      return;
     }
-    return publicURL
+    console.log(publicURL.publicUrl);
+    setPicture(publicURL.publicUrl);
+    return publicURL;
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (inputs.newPassword !== inputs.confirmPassword) {
-      alert('새 비밀번호가 일치하지 않습니다!');
-      return;
-    }
-
-    const fileElement = document.getElementById('profilePicture')
-    const file = fileElement.files[0]
-    const profilePictureUrl = await handleChangeProfile(file)
+    const fileElement = document.getElementById('profilePicture');
+    const file = fileElement.files[0];
+    const profilePictureUrl = await handleChangeProfile(file);
 
     if (profilePictureUrl) {
       inputs.profilePicture = profilePictureUrl;
     }
-
   };
   //supabase 로직을 사용(update 또는 edit)하여 변경사항 가능하게 적용
   const handleChangeProfile = async () => {
-    const {data, error} = await supabase
-    .from('Users')
-    .upsert({id:uuidv4(), name})
+    console.log(currentPassword);
+    console.log(user.id);
+    console.log(picture);
+    const { data, error } = await supabase
+      .from('Users')
+      .update({
+        name: name,
+        nickname: nickname,
+        password: currentPassword,
+        reason: mylogReason,
+        imageUrl: picture
+      })
+      .eq('userId', user.id);
 
     if (error) {
-      console.error('업데이트에 문제가 발생했습니다.', error)
+      console.error('업데이트에 문제가 발생했습니다.', error);
     } else {
-      console.log('업데이트가 완료 됐습니다!', data)
+      console.log('업데이트가 완료 됐습니다!', data);
     }
-  }
+  };
 
   // 뒤로가기
   const handleBack = () => {
-    navigate(-1); 
+    navigate(-1);
   };
 
   // 탈퇴하기
   const handleDeleteAccount = async () => {
-    const { error } = await supabase
-    .from('Users')
-    .delete()
-    .eq('id', user.id);
+    const { error } = await supabase.from('Users').delete().eq('id', user.id);
 
     if (error) {
       alert('계정 삭제 중 오류가 발생했습니다: ' + error.message);
@@ -128,11 +121,11 @@ function EditProfile() {
         </div>
         <div>
           <label htmlFor="name">이름:</label>
-          <input type="text" id="name" name="name" value={inputs.name} onChange={handleChange} />
+          <input type="text" id="name" name="name" value={name} onChange={handleOnChange} />
         </div>
         <div>
           <label htmlFor="nickname">닉네임:</label>
-          <input type="text" id="nickname" name="nickname" value={inputs.nickname} onChange={handleChange} />
+          <input type="text" id="nickname" name="nickname" value={nickname} onChange={handleOnChange} />
         </div>
         <div>
           <label htmlFor="currentPassword">현재 비밀번호:</label>
@@ -140,45 +133,17 @@ function EditProfile() {
             type="password"
             id="currentPassword"
             name="currentPassword"
-            value={inputs.currentPassword}
-            onChange={handleChange}
+            value={currentPassword}
+            onChange={handleOnChange}
           />
         </div>
-        <div>
-          <label htmlFor="newPassword">새 비밀번호:</label>
-          <input
-            type="password"
-            id="newPassword"
-            name="newPassword"
-            value={inputs.newPassword}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">새 비밀번호 확인:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={inputs.confirmPassword}
-            onChange={handleChange}
-          />
-        </div>
+
         <div>
           <label htmlFor="reasonToUseBlog">Mylog 운영 이유:</label>
-          <input
-            type="text"
-            id="reasonToBlog"
-            name="mylogReason"
-            value={inputs.mylogReason}
-            onChange={handleChange} />
+          <input type="text" id="mylogReason" name="mylogReason" value={mylogReason} onChange={handleOnChange} />
         </div>
         {status === 'failed' && <p>{error}</p>}
-        <button
-          type="submit"
-          style={{ color: 'blue' }}
-          onClick={handleChangeProfile}
-          >          
+        <button type="submit" style={{ color: 'blue' }} onClick={handleChangeProfile}>
           변경 완료
         </button>
       </form>
