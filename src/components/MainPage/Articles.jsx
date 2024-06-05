8eed38031a0e3bdbc234a3be3d57a9ed25249918
Joohwan Seo <songqkaoswe@naver.com>
2024-06-05 10:16:08 +0900

    error/ isAction problem

--- src/components/MainPage/Articles.jsx
+++ src/components/MainPage/Articles.jsx
@@ -134,10 +134,10 @@ const Articles = ({ mode }) => {
       <Header onSearch={handleSearch} />
       <Content>
         <MainSort>
-          <NavLink to="/newest" isactive={() => mode === 'newest'}>
+          <NavLink to="/newest" isActive={() => mode === 'newest'}>
             <SortButton selected={mode === 'newest'}>Newest</SortButton>
           </NavLink>
-          <NavLink to="/popular" isactive={() => mode === 'popular'}>
+          <NavLink to="/popular" isActive={() => mode === 'popular'}>
             <SortButton selected={mode === 'popular'}>Popular</SortButton>
           </NavLink>
         </MainSort>
