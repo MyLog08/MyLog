efb1866ad6de051d77510da5657ea15bb801f5d3
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-06-06 18:35:03 +0900

    style: not found page

--- src/pages/ErrorPage/NotFoundPage.jsx
+++ src/pages/ErrorPage/NotFoundPage.jsx
@@ -1,7 +1,9 @@
+import { NotFoundPageTitle } from '../../styles/ErrorPage/NotFoundPageStyle';
+
 const NotFound = () => {
   return (
     <div>
-      <h1>404 Not Found</h1>
+      <NotFoundPageTitle>404 Not Found</NotFoundPageTitle>
       <p>페이지를 찾을 수 없습니다.</p>
     </div>
   );
---
+++ src/styles/ErrorPage/NotFoundPageStyle.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const NotFoundPageTitle = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  text-align: center;
+`;
